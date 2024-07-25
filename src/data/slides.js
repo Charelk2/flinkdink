025391b19ade52d071490e6a402249302f019ce5
Charel <macbookair@MacBook-Air.local>
2024-07-25 20:22:02 +0200

    Initial commit

---
+++ src/data/slides.js
@@ -0,0 +1,197 @@
+const slides = [
+  {
+    term: 1,
+    week: 1,
+    headerTitle: "Onion",
+    title: "Ui",
+    description:
+      "1. Tlfuric compounds in onions cause is to cry when we chop them. 2. The largest onion ever grown is a whopping 4.5kg",
+    imageUrl: "/images/Term 1/Week 1/Enc/ui.jpg",
+  },
+  {
+    term: 1,
+    week: 1, // Specify the week
+    headerTitle: "Beetroot",
+    title: "Beet",
+    description:
+      "Beet roots and stems are edible and a good source of vitamin A, C, folate, calcium, and fiber. They contain many antioxidants such as betalains, carotenoids, and flavonoids which prevent colon cancer and protect heart health. Beet juice can be used as a red dye. Beets are also known as the blood turnip.",
+    imageUrl: "/images/Term 1/Week 1/Enc/beet.jpg",
+  },
+  {
+    term: 1,
+    week: 1, // Specify the week
+    headerTitle: "Greenbeans",
+    title: "Groenbone",
+    description:
+      "Beet roots and stems are edible and a good source of vitamin A, C, folate, calcium, and fiber. They contain many antioxidants such as betalains, carotenoids, and flavonoids which prevent colon cancer and protect heart health. Beet juice can be used as a red dye. Beets are also known as the blood turnip.",
+    imageUrl: "/images/Term 1/Week 1/Enc/groenbone.jpg",
+  },
+  {
+    term: 1,
+    week: 1, // Specify the week
+    headerTitle: "Sweet Potato",
+    title: "Patat",
+    description:
+      "Beet roots and stems are edible and a good source of vitamin A, C, folate, calcium, and fiber. They contain many antioxidants such as betalains, carotenoids, and flavonoids which prevent colon cancer and protect heart health. Beet juice can be used as a red dye. Beets are also known as the blood turnip.",
+    imageUrl: "/images/Term 1/Week 1/Enc/patat.jpg",
+  },
+  {
+    term: 1,
+    week: 1, // Specify the week
+    headerTitle: "Baby Marrow",
+    title: "Murgpampoen",
+    description:
+      "Beet roots and stems are edible and a good source of vitamin A, C, folate, calcium, and fiber. They contain many antioxidants such as betalains, carotenoids, and flavonoids which prevent colon cancer and protect heart health. Beet juice can be used as a red dye. Beets are also known as the blood turnip.",
+    imageUrl: "/images/Term 1/Week 1/Enc/murgpampoen.jpg",
+  },
+  {
+    term: 1,
+    week: 1, // Specify the week
+    headerTitle: "Aubergine",
+    title: "Eiervrug",
+    description:
+      "Beet roots and stems are edible and a good source of vitamin A, C, folate, calcium, and fiber. They contain many antioxidants such as betalains, carotenoids, and flavonoids which prevent colon cancer and protect heart health. Beet juice can be used as a red dye. Beets are also known as the blood turnip.",
+    imageUrl: "/images/Term 1/Week 1/Enc/eiervrug.jpg",
+  },
+  {
+    term: 1,
+    week: 1, // Specify the week
+    headerTitle: "Pumpkin",
+    title: "Pampoen",
+    description:
+      "Beet roots and stems are edible and a good source of vitamin A, C, folate, calcium, and fiber. They contain many antioxidants such as betalains, carotenoids, and flavonoids which prevent colon cancer and protect heart health. Beet juice can be used as a red dye. Beets are also known as the blood turnip.",
+    imageUrl: "/images/Term 1/Week 1/Enc/pampoen.jpg",
+  },
+  {
+    term: 1,
+    week: 1, // Specify the week
+    headerTitle: "Carrot",
+    title: "Wortel",
+    description:
+      "Beet roots and stems are edible and a good source of vitamin A, C, folate, calcium, and fiber. They contain many antioxidants such as betalains, carotenoids, and flavonoids which prevent colon cancer and protect heart health. Beet juice can be used as a red dye. Beets are also known as the blood turnip.",
+    imageUrl: "/images/Term 1/Week 1/Enc/wortel.jpg",
+  },
+  {
+    term: 1,
+    week: 1, // Specify the week
+    headerTitle: "Cauliflower",
+    title: "Blomkool",
+    description:
+      "Beet roots and stems are edible and a good source of vitamin A, C, folate, calcium, and fiber. They contain many antioxidants such as betalains, carotenoids, and flavonoids which prevent colon cancer and protect heart health. Beet juice can be used as a red dye. Beets are also known as the blood turnip.",
+    imageUrl: "/images/Term 1/Week 1/Enc/blomkool.jpg",
+  },
+  {
+    term: 1,
+    week: 1, // Specify the week
+    headerTitle: "Potato",
+    title: "Aartappel",
+    description:
+      "Beet roots and stems are edible and a good source of vitamin A, C, folate, calcium, and fiber. They contain many antioxidants such as betalains, carotenoids, and flavonoids which prevent colon cancer and protect heart health. Beet juice can be used as a red dye. Beets are also known as the blood turnip.",
+    imageUrl: "/images/Term 1/Week 1/Enc/aartappel.jpg",
+  },
+  //TAALONTWIKKELING=====
+  {
+    term: 1,
+    week: 1, // Specify the week
+    imageUrl: "/images/Term 1/Week 1/LD/1.png",
+  },
+  {
+    term: 1,
+    week: 1, // Specify the week
+    imageUrl: "/images/Term 1/Week 1/LD/2.png",
+  },
+  {
+    term: 1,
+    week: 1, // Specify the week
+    imageUrl: "/images/Term 1/Week 1/LD/3.png",
+  },
+  {
+    term: 1,
+    week: 1, // Specify the week
+    imageUrl: "/images/Term 1/Week 1/LD/4.png",
+  },
+  {
+    term: 1,
+    week: 1, // Specify the week
+    imageUrl: "/images/Term 1/Week 1/LD/5.png",
+  },
+  {
+    term: 1,
+    week: 1, // Specify the week
+    imageUrl: "/images/Term 1/Week 1/LD/6.png",
+  },
+  {
+    term: 1,
+    week: 1, // Specify the week
+    imageUrl: "/images/Term 1/Week 1/LD/7.png",
+  },
+  {
+    term: 1,
+    week: 1, // Specify the week
+    imageUrl: "/images/Term 1/Week 1/LD/8.png",
+  },
+  {
+    term: 1,
+    week: 1, // Specify the week
+    imageUrl: "/images/Term 1/Week 1/LD/9.png",
+  },
+  {
+    term: 1,
+    week: 1, // Specify the week
+    imageUrl: "/images/Term 1/Week 1/LD/10.png",
+  },
+
+  //WISKUNDE
+  {
+    term: 1,
+    week: 1, // Specify the week
+    imageUrl: "/images/Term 1/Week 1/Math/1.jpg",
+  },
+  {
+    term: 1,
+    week: 1, // Specify the week
+    imageUrl: "/images/Term 1/Week 1/Math/2.jpg",
+  },
+  {
+    term: 1,
+    week: 1, // Specify the week
+    imageUrl: "/images/Term 1/Week 1/Math/3.jpg",
+  },
+  {
+    term: 1,
+    week: 1, // Specify the week
+    imageUrl: "/images/Term 1/Week 1/Math/4.jpg",
+  },
+  {
+    term: 1,
+    week: 1, // Specify the week
+    imageUrl: "/images/Term 1/Week 1/Math/5.jpg",
+  },
+  {
+    term: 1,
+    week: 1, // Specify the week
+    imageUrl: "/images/Term 1/Week 1/Math/6.jpg",
+  },
+  {
+    term: 1,
+    week: 1, // Specify the week
+    imageUrl: "/images/Term 1/Week 1/Math/7.jpg",
+  },
+  {
+    term: 1,
+    week: 1, // Specify the week
+    imageUrl: "/images/Term 1/Week 1/Math/8.jpg",
+  },
+  {
+    term: 1,
+    week: 1, // Specify the week
+    imageUrl: "/images/Term 1/Week 1/Math/9.jpg",
+  },
+  {
+    term: 1,
+    week: 1, // Specify the week
+    imageUrl: "/images/Term 1/Week 1/Math/10.jpg",
+  },
+];
+
+export default slides;
